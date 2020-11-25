abd60aa627324d5ad614ae360b70fb14461bef09
neusiboi <74351655+neusiboi@users.noreply.github.com>
2020-11-25 11:46:24 +0100

    Update Aufgabe 2.py

--- Blatt10/Aufgabe 2.py
+++ Blatt10/Aufgabe 2.py
@@ -1,7 +1,44 @@
-from math import sin, cos
-from scipy.integrate import solve_ivp
-def rhs(s, v): 
-    return [-12*v[2]**2, 12*v[2]**2, 6*v[0]*v[2] - 6*v[2]*v[1] - 36*v[2]]
+from math import sin
+import numpy as np
 
-res = solve_ivp(rhs, (0, 0.1), [2, 3, 4])
-print(res)
+def dgl(y,t):
+    q, p = y
+    dydt = [p , -sin(q) ]
+    return dydt
+
+
+
+y1 = [-1/2,3]
+y2 = [1/2, 3]
+y3 = [1/2 , 1]
+y4 = [-1/2 ,1]
+
+
+
+
+
+
+
+t = np.linspace(1, 1.1 ,100) 
+
+from scipy.integrate import odeint
+def Solutions(y):
+    sol = odeint(dgl, y, t)
+    return sol
+
+sol1 = Solutions(y1)
+sol2 = Solutions(y2)
+sol3 = Solutions(y3)
+sol4 = Solutions(y4)
+
+import matplotlib.pyplot as plt
+
+plt.plot(sol1[:, 0],sol1[:, 1], linewidth = 5)
+plt.plot(sol2[:, 0],sol2[:, 1], linewidth = 5)
+plt.plot(sol3[:, 0],sol3[:, 1], linewidth = 5)
+plt.plot(sol4[:, 0],sol4[:, 1], linewidth = 5)
+
+plt.xlabel('q')
+plt.ylabel('p')
+plt.grid() 
+plt.show()
